656a72ed4f3179e3daf1adcd4c05b1f03d0eb313
jiangtaoliu <jet.lau@gmail.com>
2020-11-03 23:38:27 +1300

    66.js

--- 66.加一.js
+++ 66.加一.js
@@ -10,7 +10,24 @@
  * @return {number[]}
  */
 var plusOne = function(digits) {
-
+    for(var i = digits.length-1; i>=0; i--) {
+        if(digits[i] == 9) {
+            digits[i] = 0;
+            if(i == 0) {
+                digits.splice(0,0,1);
+                break;
+            }
+            continue;
+        } else {
+            digits[i] += 1;
+            break;
+        }
+    }
+    return digits;
 };
 // @lc code=end
 
+
+// @after-stub-for-debug-begin
+module.exports = plusOne;
+// @after-stub-for-debug-end
